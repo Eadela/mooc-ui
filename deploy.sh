ab2a114b026c8338e87ca579c5e9afb10c8773ca
zhiqing.wang <zhiqing.wang@brgroup.com>
2021-12-15 15:49:16 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Eadela/mook-ui.git master:gh-pages
-
+# git push -f https://github.com/Eadela/mooc-ui.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/Eadela/mooc-ui.git main:gh-pages
 cd -
